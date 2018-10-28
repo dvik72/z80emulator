2b3f695a52e0d1cc26043f4278cb20a2796ad2fc
dvik72 <daniel@vik.cc>
2018-10-27 23:24:12 -0700

    Added initial PSG emulation

---
+++ src/audio/ay8910.ts
@@ -0,0 +1,96 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and / or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+import { IoManager, Port } from '../core/iomanager';
+
+export enum Ay8910ConnectorType { MSX, SCCPLUS, SVI };
+
+const regMask = [
+  0xff, 0x0f, 0xff, 0x0f, 0xff, 0x0f, 0x1f, 0x3f,
+  0x1f, 0x1f, 0x1f, 0xff, 0xff, 0x0f, 0xff, 0xff
+];
+
+export class Ay8910 {
+  constructor(
+    private ioManager: IoManager,
+    connectorType: Ay8910ConnectorType,
+    private readCb?: (port: number) => number,
+    private writeCb?: (port: number, value: number) => void
+  ) {
+    this.writeAddress = this.writeAddress.bind(this);
+    this.writeData = this.writeData.bind(this);
+    this.readData = this.readData.bind(this);
+
+    switch (connectorType) {
+      case Ay8910ConnectorType.MSX:
+        this.ioManager.registerPort(0xa0, new Port(undefined, this.writeAddress));
+        this.ioManager.registerPort(0xa1, new Port(undefined, this.writeData));
+        this.ioManager.registerPort(0xa2, new Port(this.readData, undefined));
+        break;
+      case Ay8910ConnectorType.SCCPLUS:
+        this.ioManager.registerPort(0x10, new Port(undefined, this.writeAddress));
+        this.ioManager.registerPort(0x11, new Port(undefined, this.writeData));
+        this.ioManager.registerPort(0x12, new Port(this.readData, undefined));
+        break;
+      case Ay8910ConnectorType.SVI:
+        this.ioManager.registerPort(0x88, new Port(undefined, this.writeAddress));
+        this.ioManager.registerPort(0x8c, new Port(undefined, this.writeData));
+        this.ioManager.registerPort(0x90, new Port(this.readData, undefined));
+        break;
+    }
+  }
+
+  public reset(): void {
+    for (let i = 0; i < 16; i++) {
+      this.writeAddress(0, i);
+      this.writeData(1, 0);
+    }
+  }
+
+  private writeAddress(unusedAddress: number, value: number): void {
+    this.address = value & 0x0f;
+  }
+
+  private writeData(unusedAddress: number, value: number): void {
+    value &= regMask[this.address];
+
+    this.regs[this.address] = value;
+
+    switch (this.address) {
+      case 14:
+      case 15:
+        if (this.writeCb) {
+          const port = value & 1;
+          this.writeCb(port, value);
+        }
+    }
+  }
+
+  private readData(unusedAddress: number): number {
+    if (this.address >= 14) {
+      const port = this.address & 1;
+      if (this.readCb) {
+        this.regs[this.address] = this.readCb(port);
+      }
+    }
+    return this.regs[this.address];    
+  }
+
+  private regs = new Array<number>(16);
+  private address = 0;
+}
---
+++ src/io/msxpsg.ts
@@ -0,0 +1,82 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and / or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+import { IoManager, Port } from '../core/iomanager';
+import { Ay8910, Ay8910ConnectorType } from '../audio/ay8910';
+
+export class MsxPsg {
+  constructor(
+    private ioManager: IoManager,
+    private maxPorts : number,
+    private readCassetteLIne?: () => number
+  ) {
+    this.readIo = this.readIo.bind(this);
+    this.writeIo = this.writeIo.bind(this);
+
+    this.ay8910 = new Ay8910(this.ioManager, Ay8910ConnectorType.MSX, this.readIo, this.writeIo);
+  }
+
+  public reset(): void {
+    this.joystickPort  = 0;
+    this.regs[0] = 0;
+    this.regs[1] = 0;
+    this.readValue[0] = 0;
+    this.readValue[1] = 0;
+
+    this.ay8910.reset();
+  }
+
+  private readIo(address: number): number {
+    if (address & 1) {
+      return this.regs[1];
+    }
+
+    /* joystick pins */
+    const renshaSpeed = 0; // TODO: Connect user configruable switch
+    let state = 0x3f;
+
+    // TODO: Hook up joystick controllers
+
+    /* ANSI/JIS */
+    state |= 0x40;
+
+    /* cas signal */
+    // Call cassette Callback (for coin select
+    if (this.readCassetteLIne && this.readCassetteLIne()) {
+      state |= 0x80;
+    }
+
+    this.readValue[address & 1] = state;
+
+    return state;
+  }
+
+  private writeIo(address: number, value: number): void {
+    if (address & 1) {
+      this.joystickPort = (value >> 6) & 0x01;
+
+      // TODO: Set Kana led if value & 0x80
+    }
+    this.regs[address & 1] = value;
+  }
+
+  private ay8910: Ay8910;
+  private joystickPort = 0;
+  private regs = new Array<number>(2);
+  private readValue = new Array<number>(2);
+}
