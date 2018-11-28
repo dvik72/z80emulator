69816a9e87f56ca44ca76bf2356c8cab75391369
dvik72 <daniel@vik.cc>
2018-11-27 22:15:51 -0800

    Added TC8566AF disk emulation, added disk manager

---
+++ src/nano/diskmanager.ts
@@ -0,0 +1,43 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and / or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+import { Disk } from '../disk/disk';
+
+export class DiskManager {
+  public constructor() {
+    for (let i = 0; i < this.floppyDisks.length; i++) {
+      this.floppyDisks[i] = new Disk();
+    }
+  }
+
+  public getFloppyDisk(index: number): Disk {
+    return this.floppyDisks[index % this.floppyDisks.length];
+  }
+
+  public insertFloppyImage(index: number, image: Uint8Array): void {
+    if (index < this.floppyDisks.length) {
+      this.floppyDisks[index].load(image);
+    }
+  }
+
+  public ejectFloppyImage(index: number): void {
+    this.floppyDisks[index].unload();
+  }
+
+  private floppyDisks = new Array<Disk>(4);
+}
