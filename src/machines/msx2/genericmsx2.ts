01ec4c157a92ba54a60c9b161cde237d3be60fad
dvik72 <daniel@vik.cc>
2018-12-25 02:27:46 -0800

    Cleaned up MSX2 generic machine and added back msx music

--- src/machines/msx2/genericmsx2.ts
+++ src/machines/msx2/genericmsx2.ts
@@ -22,7 +22,6 @@ import { DiskManager } from '../../disk/diskmanager';
 
 import { MapperRamMapped } from '../../mappers/rammapped';
 import { MapperRomNormal } from '../../mappers/romnormal';
-import { MapperRomTc8566af } from '../../mappers/romTc8566af';
 import { MapperRomPhilipsFdc } from '../../mappers/romphilipsfdc';
 import { MapperMsxMusic } from '../../mappers/rommsxmusic';
 
@@ -39,7 +38,7 @@ export class GenericMsx2 extends Msx2Base {
       GenericMsx2.NAME,
       webAudio,
       diskManager,
-      ['msx2bios', 'msx2ext', 'panasonicdisk', 'philipsdisk', 'msx2pmus']);
+      ['msx2bios', 'msx2ext', 'philipsdisk', 'msx2pmus']);
   }
 
   public init(): void {
@@ -52,9 +51,8 @@ export class GenericMsx2 extends Msx2Base {
     // Configure slots
     new MapperRomNormal(this.getBoard(), 0, 0, 0, this.getSystemRom('msx2bios'));
     new MapperRomNormal(this.getBoard(), 3, 1, 0, this.getSystemRom('msx2ext'));
-    //new MapperRomTc8566af(this.getDiskManager(), this.getBoard(), 3, 1, this.getSystemRom('panasonicdisk'));
     new MapperRomPhilipsFdc(this.getDiskManager(), this.getBoard(), 3, 1, this.getSystemRom('philipsdisk'));
     new MapperRamMapped(this.getBoard(), 3, 2, 512 * 1024);
-    //new MapperMsxMusic(this.getBoard(), 3, 3, this.getSystemRom('msx2pmus'));
+    new MapperMsxMusic(this.getBoard(), 3, 3, this.getSystemRom('msx2pmus'));
   }
 }
