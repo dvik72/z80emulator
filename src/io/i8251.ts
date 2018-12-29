bc04bce48793e49ce1b3bf9131c873fce3cdde86
dvik72 <daniel@vik.cc>
2018-12-29 00:14:39 -0800

    Add MSX midi emulation to make turbo r games run correctly

---
+++ src/io/i8251.ts
@@ -0,0 +1,382 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and / or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+import { Timer } from '../core/timeoutmanager';
+import { Board } from '../core/board';
+
+const STAT_TXRDY = 0x01;
+const STAT_RXRDY = 0x02;
+const STAT_TXEMPTY = 0x04;
+const STAT_PE = 0x08;
+const STAT_OE = 0x10;
+const STAT_FE = 0x20;
+const STAT_SYNBRK = 0x40;
+const STAT_DSR = 0x80;
+
+const MODE_BAUDRATE = 0x03;
+const MODE_SYNCHRONOUS = 0x00;
+const MODE_RATE1 =  0x01
+const MODE_RATE16 = 0x02;
+const MODE_RATE64 = 0x03;
+const MODE_WORDLENGTH = 0x0c;
+const MODE_5BIT =   0x00
+const MODE_6BIT = 0x04;
+const MODE_7BIT = 0x08;
+const MODE_8BIT = 0x0c;
+const MODE_PARITYEN = 0x10;
+const MODE_PARITODD = 0x00;
+const MODE_PARITEVEN = 0x20;
+const MODE_STOP_BITS = 0xc0;
+const MODE_STOP_INV = 0x00;
+const MODE_STOP_1 = 0x40;
+const MODE_STOP_15 = 0x80;
+const MODE_STOP_2 = 0xc0;
+const MODE_SINGLESYNC = 0x80;
+
+const CMD_TXEN = 0x01;
+const CMD_DTR = 0x02;
+const CMD_RXE = 0x04;
+const CMD_SBRK = 0x08;
+const CMD_RSTERR = 0x10;
+const CMD_RTS = 0x20;
+const CMD_RESET = 0x40;
+const CMD_HUNT = 0x80;
+
+const PHASE_MODE = 0;
+const PHASE_SYNC1 = 1;
+const PHASE_SYNC2 = 2;
+const PHASE_CMD = 3;
+
+const RX_QUEUE_SIZE = 16;
+
+export enum I8251Parity { EVEN, ODD, NONE };
+
+export class I8251 {
+  constructor(
+    private board: Board,
+    private frequency: number,
+    private transmit?: (value: number) => void,
+    private signal?: () => void,
+    private setDataBits?: (value: number) => void,
+    private setStopBits?: (value: number) => void,
+    private setParity?: (parity: I8251Parity) => void,
+    private setRxReady?: (value: number) => void,
+    private setDtr?: (value: number) => void,
+    private setRts?: (value: number) => void,
+    private getDtr?: () => boolean,
+    private getRts?: () => boolean
+  ) {
+    this.timerRecv = this.board.getTimeoutManager().createTimer(name, this.onRecv.bind(this));
+    this.timerRxPoll = this.board.getTimeoutManager().createTimer(name, this.onRxPoll.bind(this));
+    this.timerTrans = this.board.getTimeoutManager().createTimer(name, this.onTrans.bind(this));
+  }
+
+  public reset() {
+    this.charLength = 1024;
+    this.rxPending = 0;
+
+    this.status = STAT_TXRDY | STAT_TXEMPTY;
+    this.command = 0xff;
+    this.writeCommand(0);
+    this.cmdFaze = PHASE_MODE;
+  }
+
+  public write(port: number, value: number): void {
+    switch (port & 1) {
+      case 0:
+        this.writeTrans(value);
+        break;
+      case 1:
+        switch (this.cmdFaze) {
+          case PHASE_MODE:
+            this.setMode(value);
+            if ((this.mode & MODE_BAUDRATE) == MODE_SYNCHRONOUS) {
+              this.cmdFaze = PHASE_SYNC1;
+            }
+            else {
+              this.cmdFaze = PHASE_CMD;
+            }
+            break;
+          case PHASE_SYNC1:
+            this.sync1 = value;
+            if (this.mode & MODE_SINGLESYNC) {
+              this.cmdFaze = PHASE_CMD;
+            }
+            else {
+              this.cmdFaze = PHASE_SYNC2;
+            }
+            break;
+          case PHASE_SYNC2:
+            this.sync2 = value;
+            this.cmdFaze = PHASE_CMD;
+            break;
+          case PHASE_CMD:
+            if (value & CMD_RESET) {
+              this.cmdFaze = PHASE_MODE;
+            }
+            else {
+              this.writeCommand(value);
+            }
+            break;
+        }
+        break;
+    }
+  }
+
+  public read(port: number): number {
+    switch (port & 1) {
+      case 0:
+        return this.readTrans();
+      case 1:
+        return this.readStatus();
+    }
+    return 0xff;
+  }
+
+  public rxData(value: number): void {
+    if (this.rxPending < RX_QUEUE_SIZE) {
+      this.rxQueue[this.rxHead & (RX_QUEUE_SIZE - 1)] = value;
+      this.rxHead++;
+      this.rxPending++;
+    }
+    else {
+      this.status |= STAT_OE;
+    }
+  }
+
+  private writeTrans(value: number): void {
+    if (!(this.command & CMD_TXEN)) {
+      return;
+    }
+    if (this.status & STAT_TXEMPTY) {
+      this.status &= ~STAT_TXEMPTY;
+      this.sendByte = value;
+
+      this.timeTrans = this.board.getSystemTime() +
+        this.board.getSystemFrequency() * this.charLength / this.frequency | 0;
+      this.timerTrans.setTimeout(this.timeTrans);
+    }
+    else {
+      this.sendBuffer = value;
+      this.status &= ~STAT_TXRDY;
+    }
+  }
+
+  private readTrans(): number {
+    this.status &= ~STAT_RXRDY;
+    this.setRxReady && this.setRxReady(0);
+    return this.recvBuf;
+  }
+
+  private readStatus(): number {
+    let result = this.status;
+    if (this.getDtr && this.getDtr()) {
+      result |= STAT_DSR;
+    }
+    return result;
+  }
+
+  private writeCommand(value: number): void {
+    const oldCommand = this.command;
+    this.command = value;
+
+    this.setRts && this.setRts(value & CMD_RTS);
+    this.setDtr && this.setDtr(value & CMD_DTR);
+
+    if (!(value & CMD_TXEN)) {
+      this.timerTrans.stop();
+      this.status |= STAT_TXRDY | STAT_TXEMPTY;
+    }
+    if (value & CMD_RSTERR) {
+      this.status &= ~(STAT_PE | STAT_OE | STAT_FE);
+    }
+    if ((value ^ oldCommand) & CMD_RXE) {
+      if (value & CMD_RXE) {
+        this.status &= ~(STAT_PE | STAT_OE | STAT_FE);
+        this.recvReady = 1;
+        this.rxPending = 0;
+        this.onRxPoll(this.board.getSystemTime());
+      }
+      else {
+        this.timerRecv.stop();
+        this.timerRxPoll.stop();
+        this.status &= ~(STAT_PE | STAT_OE | STAT_FE);
+        this.status &= ~STAT_RXRDY;
+      }
+      this.signal && this.signal();
+    }
+  }
+
+  private setMode(value: number): void {
+    let baudrate = 1;
+    let stopBits = 0;
+    let dataBits = 8;
+    let parityEnable = 1;
+
+    this.mode = value;
+
+    switch (value & MODE_WORDLENGTH) {
+      case MODE_5BIT:
+        dataBits = 5;
+        break;
+      case MODE_6BIT:
+        dataBits = 6;
+        break;
+      case MODE_7BIT:
+        dataBits = 7;
+        break;
+      case MODE_8BIT:
+        dataBits = 8;
+        break;
+    }
+
+    this.setDataBits && this.setDataBits(dataBits);
+
+    switch (value & MODE_STOP_BITS) {
+      case MODE_STOP_INV:
+        stopBits = 0;
+        break;
+      case MODE_STOP_1:
+        stopBits = 2;
+        break;
+      case MODE_STOP_15:
+        stopBits = 3;
+        break;
+      case MODE_STOP_2:
+        stopBits = 4;
+        break;
+    }
+
+    this.setStopBits && this.setStopBits(stopBits);
+
+    switch (value & (MODE_PARITYEN | MODE_PARITEVEN)) {
+      case MODE_PARITYEN | MODE_PARITEVEN:
+        this.setParity && this.setParity(I8251Parity.EVEN);
+        break;
+      case MODE_PARITYEN:
+        this.setParity && this.setParity(I8251Parity.ODD);
+        break;
+      default:
+        this.setParity && this.setParity(I8251Parity.NONE);
+    }
+
+    switch (value & MODE_BAUDRATE) {
+      case MODE_SYNCHRONOUS:
+        baudrate = 1;
+        break;
+      case MODE_RATE1:
+        baudrate = 1;
+        break;
+      case MODE_RATE16:
+        baudrate = 16;
+        break;
+      case MODE_RATE64:
+        baudrate = 64;
+        break;
+    }
+
+    parityEnable = (value & MODE_PARITYEN) ? 1 : 0;
+    this.charLength = (((2 * (1 + dataBits + parityEnable)) + stopBits) * baudrate) >> 1;
+  }
+
+
+  private onRecv(time: number): void {
+    this.timeRecv  = 0;
+    this.recvReady = 1;
+    this.signal && this.signal();
+
+    this.onRxPoll(time);
+  }
+
+  private onRxPoll(time: number): void {
+    let value = 0;
+
+    if (this.timeRxPoll != 0) {
+      this.timerRxPoll.stop();
+      this.timeRxPoll = 0;
+    }
+
+    if (this.rxPending == 0) {
+      this.timeRxPoll = this.board.getSystemTime() +
+        this.board.getSystemFrequency() * this.charLength / this.frequency | 0;
+      this.timerRxPoll.setTimeout(this.timeRxPoll);
+      return;
+    }
+
+    if (this.rxPending != 0) {
+      value = this.rxQueue[(this.rxHead - this.rxPending) & (RX_QUEUE_SIZE - 1)];
+      this.rxPending--;
+    }
+    this.recvBuf = value;
+    this.status |= STAT_RXRDY;
+    this.setRxReady && this.setRxReady(1);
+    this.recvReady = 0;
+
+    this.timeRecv = this.board.getSystemTime() +
+      this.board.getSystemFrequency() * this.charLength / this.frequency | 0;
+    this.timerRecv.setTimeout(this.timeRecv);
+  }
+
+  private onTrans(time: number): void {
+    this.timeTrans  = 0;
+
+    this.transmit && this.transmit(this.sendByte);
+    if (this.status & STAT_TXRDY) {
+      this.status |= STAT_TXEMPTY;
+    }
+    else {
+      this.status |= STAT_TXRDY;
+      this.status &= ~STAT_TXEMPTY;
+      this.sendByte = this.sendBuffer;
+
+      this.timeTrans = this.board.getSystemTime() +
+        this.board.getSystemFrequency() * this.charLength / this.frequency | 0;
+      this.timerTrans.setTimeout(this.timeTrans);
+    }
+  }
+
+  private timeRecv = 0;
+  private timeRxPoll = 0;
+  private timeTrans = 0;
+
+  private status = 0;
+  private command = 0;
+  private mode = 0;
+  private sync1 = 0;
+  private sync2 = 0;
+  private charLength = 0;
+  private cmdFaze = 0;
+
+  private dataBits = 0;
+  private stopBits = 0;
+  private parityEnabled = 0;
+  private parity = 0;
+  private recvBuf = 0;
+  private recvReady = 0;
+  private sendByte = 0;
+  private sendBuffer = 0;
+  private sendBuffered = 0;
+
+  private rxPending = 0;
+  private rxHead = 0;
+  private rxQueue = new Uint8Array(RX_QUEUE_SIZE);
+
+  private timerRecv: Timer;
+  private timerRxPoll: Timer;
+  private timerTrans: Timer;
+}
---
+++ src/io/msxmidi.ts
@@ -0,0 +1,233 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and / or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+import { Timer } from '../core/timeoutmanager';
+import { Board, InterruptVector } from '../core/board';
+import { Port } from '../core/iomanager';
+import { I8251 } from './i8251';
+import { I8254 } from './i8254';
+
+export class MsxMidi {
+  constructor(
+    private board: Board,
+    private isExternal = false
+  ) {
+    this.readIo = this.readIo.bind(this);
+    this.writeIo = this.writeIo.bind(this);
+
+    this.i8254 = new I8254(board, 4000000, undefined, undefined, this.pitOut2.bind(this));
+    this.i8251 = new I8251(
+      board, 4000000,
+      this.transmit.bind(this),
+      undefined, // signal
+      undefined, // setDataBits
+      undefined, // setStopBits
+      undefined, // setParity
+      this.setRxRdyIrq.bind(this),
+      this.enableTimerIrq.bind(this),
+      this.enableRxRdyIrq.bind(this),
+      this.getDtr.bind(this),
+      this.getRts.bind(this));
+
+    if (this.isExternal) {
+      this.board.getIoManager().registerPort(0xe2, new Port(undefined, this.writeIoE2.bind(this)));
+    }
+    else {
+      this.registerIoPorts(0xe8);
+    }
+
+    this.reset();
+  }
+
+  public reset() {
+    this.board.clearInt(InterruptVector.MIDI_TMR);
+    this.board.clearInt(InterruptVector.MIDI_RXRDY);
+
+    this.timerIrqLatch = false;
+    this.timerIrqEnabled = false;
+    this.rxRdyIrqLatch = false;
+    this.rxRdyIrqEnabled = false;
+
+    if (this.isExternal) {
+      this.unregisterIoPorts();
+    }
+
+    this.i8251.reset();
+    this.i8254.reset();
+  }
+
+  private unregisterIoPorts() {
+    if (this.ioStart == 0) {
+      return;
+    }
+
+    for (let i = 0; i < (this.ioStart == 0xe0 ? 2 : 8); i++) {
+      this.board.getIoManager().unregisterPort(this.ioStart + i);
+    }
+
+    this.ioStart = 0;
+  }
+
+  private registerIoPorts(ioStart: number): void {
+    if (this.ioStart == ioStart) {
+      return;
+    }
+
+    if (this.ioStart != 0) {
+      this.unregisterIoPorts();
+    }
+
+    this.ioStart = ioStart;
+
+    let i = this.ioStart == 0xe0 ? 2 : 8;
+    while (i--) {
+      this.board.getIoManager().registerPort(ioStart + i, new Port(this.readIo, this.writeIo));
+    }
+  }
+
+  private readIo(ioPort: number): number {
+    switch (ioPort & 7) {
+      case 0: // UART data register
+      case 1: // UART status register
+        return this.i8251.read(ioPort & 3);
+        break;
+      case 2: // timer interrupt flag off
+      case 3: // no function
+        return 0xff;
+      case 4: // counter 0 data port
+      case 5: // counter 1 data port
+      case 6: // counter 2 data port
+      case 7: // timer command register
+        return this.i8254.read(ioPort & 3);
+    }
+    return 0xff;
+  }
+
+  private writeIoE2(ioPort: number, value: number): void {
+    if ((ioPort & 0xff) == 0xe2) {
+      const ioStart = (value & 1) ? 0xe0 : 0xe8;
+      if (value & 0x80) {
+        this.unregisterIoPorts();
+      }
+      else {
+        this.registerIoPorts(ioStart);
+      }
+    }
+  }
+
+  private writeIo(ioPort: number, value: number): void {
+    switch (ioPort & 7) {
+      case 0: // UART data register
+      case 1: // UART command register
+        this.i8251.write(ioPort & 3, value);
+        break;
+      case 2: // timer interrupt flag off
+        this.setTimerIrq(false);
+        break;
+      case 3: // no function
+        break;
+      case 4: // counter 0 data port
+      case 5: // counter 1 data port
+      case 6: // counter 2 data port
+      case 7: // timer command register
+        this.i8254.write(ioPort & 3, value);
+        break;
+    }
+  }
+
+
+  private setTimerIrq(status: boolean): void {
+    if (this.timerIrqLatch != status) {
+      this.timerIrqLatch = status;
+      if (this.timerIrqEnabled) {
+        if (this.timerIrqLatch) {
+          this.board.setInt(InterruptVector.MIDI_TMR);
+        }
+        else {
+          this.board.clearInt(InterruptVector.MIDI_TMR);
+        }
+      }
+
+      this.i8254.setGate(2, this.timerIrqEnabled && !this.timerIrqLatch);
+    }
+  }
+
+  private enableTimerIrq(enabled: boolean) {
+    if (this.timerIrqEnabled != enabled) {
+      this.timerIrqEnabled = enabled;
+      if (this.timerIrqLatch) {
+        if (this.timerIrqEnabled) {
+          this.board.setInt(InterruptVector.MIDI_TMR);
+        }
+        else {
+          this.board.clearInt(InterruptVector.MIDI_TMR);
+        }
+      }
+      this.i8254.setGate(2, this.timerIrqEnabled && !this.timerIrqLatch);
+    }
+  }
+
+  private setRxRdyIrq(status: boolean): void {
+    if (this.rxRdyIrqLatch != status) {
+      this.rxRdyIrqLatch = status;
+      if (this.rxRdyIrqEnabled) {
+        if (this.rxRdyIrqLatch) {
+          this.board.setInt(InterruptVector.MIDI_RXRDY);
+        }
+        else {
+          this.board.clearInt(InterruptVector.MIDI_RXRDY);
+        }
+      }
+    }
+  }
+
+  private enableRxRdyIrq(enabled: boolean): void {
+    if (this.rxRdyIrqEnabled != enabled) {
+      this.rxRdyIrqEnabled = enabled;
+      if (!this.rxRdyIrqEnabled && this.rxRdyIrqLatch) {
+        this.board.clearInt(InterruptVector.MIDI_RXRDY);
+      }
+    }
+  }
+
+  private transmit(value: number): void {
+    //midiIoTransmit(value);
+  }
+
+  private getDtr(): boolean {
+    return this.board.getInt(InterruptVector.MIDI_TMR);
+  }
+
+  private getRts() {
+    return true;
+  }
+
+  private pitOut2(state: boolean): void {
+    this.setTimerIrq(true); // Q: should it be state??
+  }
+
+  private ioStart = 0;
+  private timerIrqLatch = false;
+  private timerIrqEnabled = false;
+  private rxRdyIrqLatch = false;
+  private rxRdyIrqEnabled = false;
+
+  private i8251: I8251;
+  private i8254: I8254;
+
+}
